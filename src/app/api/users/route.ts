82f8dc80ec1f733bfeda4356d4fda3aa9f388b62
Ab-Rehman01 <161899946+Ab-Rehman01@users.noreply.github.com>
2025-08-11 04:07:11 +0500

    main

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -7,9 +7,9 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const session = await getServerSession(authOptions);
 
-  if (!session) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
+  if (!session?.user?.email || !session.user.email.startsWith("admin")) {
+  return new Response("Unauthorized", { status: 403 });
+}
 
   const client = await clientPromise;
   const db = client.db("education-system");
