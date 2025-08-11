58eb500c23a860d40ef1812cfc7cec56009577fc
Ab-Rehman01 <161899946+Ab-Rehman01@users.noreply.github.com>
2025-08-11 17:23:16 +0500

    user list

--- src/app/api/users/list/route.ts
+++ src/app/api/users/list/route.ts
@@ -1,11 +1,17 @@
 //src/app/api/users/list/route.ts
-// src/app/api/assignments/list/route.ts
+
+//src/app/api/users/list/route.ts
 import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 
 export async function GET() {
-  const client = await clientPromise;
-  const db = client.db("education-system");
-  const classes = await db.collection("classes").find({}).toArray();
-  return NextResponse.json(classes);
+  try {
+    const client = await clientPromise;
+    const db = client.db("education-system");
+    const users = await db.collection("users").find().toArray();
+
+    return NextResponse.json(users);
+  } catch {
+    return NextResponse.json({ message: "Failed to fetch users" }, { status: 500 });
+  }
 }
